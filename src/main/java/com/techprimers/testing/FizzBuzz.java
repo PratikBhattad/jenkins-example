2b46c940c6c8cf4a670c8d8798d29456494fe266
Pratik Bhattad <pbhattad346@gmail.com>
2026-05-10 11:35:13 +0530

    first push request

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -4,6 +4,7 @@ public class FizzBuzz {
 
     public String play(int number) {
 
+        System.out.print("TEst push 1");
         if (number == 0) throw new IllegalArgumentException("Number must not be 0");
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
